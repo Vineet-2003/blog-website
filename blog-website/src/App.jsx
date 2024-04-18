6880649cd84bab6ec98b301bbe182b56f384e8b9
Vineet-2003 <107121127@nitt.edu>
2024-04-19 00:46:02 +0530

    Appwrite Configured

---
+++ blog-website/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+      <div>Blog website with React + vite </div>
+    </>
+  )
+}
+
+export default App
